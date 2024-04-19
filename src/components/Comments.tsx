64802f0c8f7f48589b916d4859ddd0803dcc87e8
Sudhan R <123430617+Sudhan-23082004@users.noreply.github.com>
2024-04-19 11:54:10 +0530

    Update Comments.tsx

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -3,19 +3,20 @@ import Giscus from '@giscus/react';
 export default function Comments() {
   return (
     <div className=" px-32 pb-24 bg-[#ffe8e8]">
-    <Giscus
+      <Giscus
       id="comments"
-      repo="Sudhan-23082004/library_comments"
-      repoId="R_kgDOLeT4sQ"
+      repo="Sudhan-23082004/Library_Management"
+      repoId="R_kgDOLf637g"
       category="Announcements"
-      categoryId="DIC_kwDOLeT4sc4Cd3UE"
+      categoryId="DIC_kwDOLf637s4Ceyne"
       mapping="pathname"
       reactionsEnabled="1"
       emitMetadata="0"
-      inputPosition="bottom"
-      theme="light"
+      inputPosition="top"
+      theme="preferred_color_scheme"
       lang="en"
       loading="lazy"
-    /></div>
+    />
+</div>
   );
 }
